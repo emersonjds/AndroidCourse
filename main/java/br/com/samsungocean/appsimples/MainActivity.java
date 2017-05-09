c33525bd2b4fb20ec97433667c67f8d2ef1c36da
Aluno <Aluno>
2017-05-09 18:57:08 -0300

    First App Lab

---
+++ main/java/br/com/samsungocean/appsimples/MainActivity.java
@@ -0,0 +1,30 @@
+package br.com.samsungocean.appsimples;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonNext = (Button)findViewById(R.id.button_next);
+        buttonNext.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this, NextActivity.class);
+        startActivity(intent);
+    }
+}
+
+
